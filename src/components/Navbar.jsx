5b4c0d72e9f4c553285c860e5a210e036e79dbeb
Allyson Mitchem <allysonmitchem@gmail.com>
2022-10-13 08:10:15 -0600

    updated boilerplate

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Navbar = () => {
+    return (
+        <div id ="navbar">
+            <h2> I am navbar</h2>
+        </div>
+    )
+
+    
+}
+
+export default Navbar;
