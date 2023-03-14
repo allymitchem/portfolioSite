1f83dd08ac59e89f5b3ef0b80af53271310c119d
Allyson Mitchem <allysonmitchem@gmail.com>
2023-03-14 11:40:29 -0600

    added routes for componenets

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,29 @@
 import React from "react";
-import {Navbar, Portfolio, Home} from './'
+import {Navbar, Portfolio, Home, About} from './'
 import {Route, Routes} from "react-router-dom"
 
 
 const Main = () => {
   return (
     <div id="main">
-    <Navbar/>
+      <Navbar/>
+      <Routes>
+        <Route path="about" 
+               element ={
+                <About/> 
+               }
+        />
+        <Route path="portfolio"
+               element ={
+                <Portfolio/>
+               }
+        />
+        <Route path="/"
+              element ={
+                <Home/>
+              }
+         />       
+      </Routes>
   </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { useNavigate, Navlink } from "react-router-dom";
 
 const Navbar = () => {
+    const navigate = useNavigate
     return (
         <div id ="navbar">
             <h2> I am navbar</h2>
