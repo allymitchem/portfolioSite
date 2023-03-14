58611939772658f2a52ab4047fdf075946cd994a
Allyson Mitchem <allysonmitchem@gmail.com>
2023-03-14 13:44:10 -0600

    added icons and links in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import email from './email.png'
+import linkedin from './linkedin.png'
+import github from './github.png'
 
 const Footer = () => {
     return (
         <div id="footer">
-            <h2 id="contact">Contact Me</h2>
+            
+            <a href="mailto:allysonmitchem@gmail.com">
+                <img id="footerIcons" src={email} alt="email"/>
+            </a>
+            <a href="https://www.linkedin.com/in/allysonmitchem/">
+                <img  id="footerIcons" src={linkedin} alt="linkedIn" />
+            </a>
+            <a href="https://github.com/allymitchem">
+                <img id="footerIcons" src={github} alt="github"/>
+            </a>
+
+      
         </div>
     )
 }
