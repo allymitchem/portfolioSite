28a12e7ea2630e05783b9328f21c6d3aa955b0d9
Allyson Mitchem <allysonmitchem@gmail.com>
2023-03-15 13:17:09 -0600

    added name to home page, added some styling to navbar, footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Home = () => {
     return (
         <div id ="home">
-            <h2> I am home</h2>
+            <h1 id="name"> Allyson Mitchem</h1>
+            <p id="title">Full Stack Web Developer </p>
+
         </div>
     )
 
