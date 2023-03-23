7868096235822465acb38c7a9ff2389e54bdc263
Allyson Mitchem <allysonmitchem@gmail.com>
2023-03-23 13:57:03 -0600

    added border, fixed alignment issue with links

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -18,13 +18,15 @@ return (
           <p className="roleDate">
             <b>Collaborator | December 2022</b>
           </p>
-          <a href="https://graceshopperfrontend.onrender.com">Deployed</a>
-          <a href="https://github.com/allymitchem/capstoneFrontEnd">
+          <div className="allLinks">
+            <a className="links" href="https://graceshopperfrontend.onrender.com">Deployed</a>
+            <a className="links" href="https://github.com/allymitchem/capstoneFrontEnd">
             Front End
-          </a>
-          <a href="https://github.com/allymitchem/capstoneBackEnd">Back End</a>
+            </a>
+          < a className="links" id="lastLink" href="https://github.com/allymitchem/capstoneBackEnd">Back End</a>
+          </div>
           <p>
-            An e-commerce website selling multiple genres of books. Users can
+            <i>Sheds & Dignity</i> is an e-commerce website selling multiple genres of books. Users can
             register and login to the website to view previous order history,
             update their information in their user account and confirm their
             purchase in the cart view.
