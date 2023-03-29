c4f7c1446e392fcabf68fd0cb17b522937d0b2ab
Allyson Mitchem <allysonmitchem@gmail.com>
2023-03-29 16:07:46 -0600

    working on formatting cards to map over, need to complete adding card info to array

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -1,56 +1,81 @@
 import React, {useState} from "react";
 
 const FlipCard = () => {
-    const [isFlipped, setIsFlipped] = useState(false)
-    const handleClick =()=> {
-        setIsFlipped(!isFlipped)
+    //need to create new id for each new card created so that flip works independently
+    const [cards, setCards] = useState([
+        {id: 1, isFlipped: false},
+        {id: 2, isFlipped: false}
+    ])
+    const handleClick =(id)=> {
+        const updatedCards = cards.map((card) => {
+            if (card.id === id){
+                return { ...card, isFlipped: !card.isFlipped}
+            }
+            return card
+        })
+        setCards(updatedCards)
     }
 return (
-    <div className={`card ${isFlipped ? "is-flipped" : ""}`} onClick={handleClick}>
+    <div className="cardGrid">
+    {cards.map((card) => (
+        <div
+            key={card.id}
+            className={`card ${card.isFlipped ? "is-flipped" : ""}`}
+            onClick={() => handleClick(card.id)}
+        >
+            <div id="cardFront">
+                <img
+                    className="siteImage"
+                    src="https://res.cloudinary.com/fsa2/image/upload/v1670130227/Site%20Images/Logo_for_Website_1_2_xnx2hf.png"
+                />
+            </div>
+            <div id="cardBack">
+                <h3 className="projectTitle">
+                    <b>Sheds & Dignity - A Bookshop</b>
+                </h3>
+                <p className="roleDate">
+                    <b>Collaborator | December 2022</b>
+                </p>
+                <div className="allLinks">
+                    <a className="links" href="https://graceshopperfrontend.onrender.com">
+                        Deployed
+                    </a>
+                    <a className="links" href="https://github.com/allymitchem/capstoneFrontEnd">
+                        Front End
+                    </a>
+                    <a className="links" id="lastLink" href="https://github.com/allymitchem/capstoneBackEnd">
+                        Back End
+                    </a>
+                </div>
+                <p>
+                    <i>Sheds & Dignity</i> is an e-commerce website selling multiple genres of books. Users can
+                    register and login to the website to view previous order history, update their information in
+                    their user account and confirm their purchase in the cart view.
+                </p>
+                <ul>
+                    <li>Developed using the PERN stack</li>
+                    <li>The backend consisted of a PostgreSQL database, and an Express implemented API</li>
+                    <li>React.js was used to build the frontend of the website</li>
+                    <li>
+                        Within our group, I focused within the users aspect of the site, both backend and
+                        frontend, while also working on the admin view and overall design of the site
+                    </li>
+                </ul>
+            </div>
+        </div>
+    ))}
+        
+      
+        
+       
+        <div  
+             className={`card ${cards[1].isFlipped ? "is-flipped" : ""}`} 
+             onClick={() => handleClick(cards[1].id)}>
         <div id="cardFront">
-           <img className="siteImage" src="https://res.cloudinary.com/fsa2/image/upload/v1670130227/Site%20Images/Logo_for_Website_1_2_xnx2hf.png"/>
+           <img className="siteImage" src="https://res.cloudinary.com/fsa2/image/upload/v1680122737/portfolio%20site%20images/tictactoe_meerot.png"/>
         </div>
         <div id="cardBack">
-        <div className="cardContainer">
-          <h3 className="projectTitle">
-            <b>Sheds & Dignity - A Bookshop</b>
-          </h3>
-          <p className="roleDate">
-            <b>Collaborator | December 2022</b>
-          </p>
-          <div className="allLinks">
-            <a className="links" href="https://graceshopperfrontend.onrender.com">Deployed</a>
-            <a className="links" href="https://github.com/allymitchem/capstoneFrontEnd">
-            Front End
-            </a>
-          < a className="links" id="lastLink" href="https://github.com/allymitchem/capstoneBackEnd">Back End</a>
-          </div>
-          <p>
-            <i>Sheds & Dignity</i> is an e-commerce website selling multiple genres of books. Users can
-            register and login to the website to view previous order history,
-            update their information in their user account and confirm their
-            purchase in the cart view.
-          </p>
-          <ul>
-            <li>Developed using the PERN stack</li>
-            <li>
-              The backend consisted of a PostgreSQL database, and an Express
-              implemented API
-            </li>
-            <li>React.js was used to build the frontend of the website</li>
-            <li>
-              Within our group, I focused within the users aspect of the site,
-              both backend and frontend, while also working on the admin view
-              and overall design of the site
-            </li>
-          </ul>
-        </div>
-        </div>
-        {/* <div id="cardFront">
-           <img className="siteImage" src=""/>
-        </div>
-        <div id="cardBack">
-        <div className="cardContainer">
+        
           <h3 className="projectTitle">
             <b></b>
           </h3>
@@ -64,22 +89,13 @@ return (
             </a>
           < a className="links" id="lastLink" href="">Back End</a>
           </div>
-          <p>
-            <i></i> 
-          </p>
-          <ul>
-            <li></li>
-            <li>
-  
-            </li>
-            <li></li>
-            <li>
-
-            </li>
-          </ul>
+          
+          
+        
         </div>
-        </div> */}
-    </div>
+     
+   </div>
+ </div>       
 )
 }
 
