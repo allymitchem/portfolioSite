ce1f2faab2060ba1b635b8b0e356dc926dcfa844
Allyson Mitchem <allysonmitchem@gmail.com>
2023-05-18 16:09:38 -0600

    added quote generator app to projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -9,6 +9,20 @@ const ProjectCard = () => {
         <div class="projectContainer">
             <div class="individualProjects">
                 <span class="imageContainer">
+                    <img src="https://res.cloudinary.com/fsa2/image/upload/v1684447120/portfolio%20site%20images/Screen_Shot_2023-05-18_at_3.58.17_PM_w2z2b3.png" alt="QuoteGenerator"/>
+                </span>
+                <h2 class="titles" >Quote Generator</h2>
+                <h3 class="roles">Developer | May 2023</h3>
+                <p class="descriptions">This quote generating app was made with HTML, CSS, and JavaScript. Fetch calls are made to an API to retrieve quotes. User capability includes generating new quotes and tweeting the displayed quote through Twitter integration.</p>
+                <span class="projectLinks">
+                <a  href="https://github.com/allymitchem/quoteGenerator" target ="_blank">
+                <img class="projectGH" src={github} alt="github"/>
+                </a>
+                </span>
+             </div>
+
+            <div class="individualProjects">
+                <span class="imageContainer">
                     <img src="https://res.cloudinary.com/fsa2/image/upload/v1684446400/portfolio%20site%20images/Screen_Shot_2023-05-18_at_3.46.00_PM_npjyyw.png" alt="Sheds&Dignity"/>
                 </span>
                 <h2 class="titles" >Sheds & Dignity - A Bookshop</h2>
@@ -20,9 +34,8 @@ const ProjectCard = () => {
                 </a>
                 </span>
              </div>
-            <div class="individualProjects">
-                <h2>projects</h2>
-            </div>
+            
+             
             
         </div>
     )
