d5d55aeb64534c03e106abde8bd86f915b3048b2
Allyson Mitchem <allysonmitchem@gmail.com>
2023-05-18 12:27:51 -0600

    played with moving background image to only home page component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 
-const Home = () => {
+const Home = ({isHomePage}) => {
+    const homeStyles = {
+        backgroundImage: isHomePage ? 'url("https://res.cloudinary.com/fsa2/image/upload/v1678901603/portfolio%20site%20images/pexels-elias-tigiser-2757549_ngaadk.jpg")' : '',
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        height: "100vh"
+        }
+
+
     return (
-        <div id ="home">
+        <div  style={homeStyles}>
+            <span id="home">
             <h1 id="name"> Allyson Mitchem</h1>
             <p id="title">Full Stack Web Developer </p>
-
+            </span>
         </div>
     )
 
