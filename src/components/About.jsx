224526891a8c4cd8c83551882e66eefc351c2a21
Allyson Mitchem <allysonmitchem@gmail.com>
2023-05-19 15:09:19 -0600

    added about me section, reworking about me with media query

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,16 @@ const About = () => {
         <div id ="about" >
             <div  >
             <h2> About Me</h2>
-            <p id="aboutMeTextBox">Cat ipsum dolor sit amet, hey! you there, with the hands and i show my fluffy belly but it's a trap! if you pet it i will tear up your hand and eat prawns daintily with a claw then lick paws clean wash down prawns with a lap of carnation milk then retire to the warmest spot on the couch to claw at the fabric before taking a catnap scream for no reason at 4 am. Kitty poochy stand in doorway, unwilling to chose whether to  </p>
+            <p id="aboutMeTextBox">
+            I am a web developer currently living in Utah, but I was born and raised in Southern California, where I received my Bachelor of Science in biology at California State Polytechnic University, Pomona. After gaining six years of experience as a medical coder, I was compelled to embark on a new career. As a recent graduate of the Full-Stack Web Development cohort from Utah State University powered through Fullstack Academy, I am excited to begin my journey through tech and expand my skills learning various programming languages. 
+            </p>
+            <p id="aboutMeTextBox">
+            As a military spouse, being able to maintain career skills that transcend geographical boundaries is extremely important. This has allowed me to acquire a sense of adaptability with my working life.
+            </p>
+            <p id="aboutMeTextBox">
+            When I am not immersed in coding, you can often me spending time with my little family. Gardening, gaming, reading, and spending time with my exceedingly spoiled cats are just a few things that help bring balance to the force.
+            </p>
+
             </div>
             <div id="langIcons">
                 
